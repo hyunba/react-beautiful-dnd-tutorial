aca703efdd477727149aa5216291dfc17ea637c6
[hyunba] <dls272@naver.com>
2022-01-23 21:37:23 +0900

    get selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState)
+  const hours = useRecoilValue(hourSelector)
+  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  }
+  // +"1" -> 1 (현재 기본 값이 number인데 input에서 넘어오는 값은 string이다 하지만 +를 앞에 붙여주면 number가 된다)
   return (
-    <div></div>
+    <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
   );
 }
 
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,15 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+export const minuteState = atom({
+    key: "minutes",
+    default: 0,
+});
+
+// selector에는 get 함수가 있다.
+export const hourSelector = selector({
+    key: "hour",
+    get: ({get}) => {
+        const minutes = get(minuteState);
+        return minutes / 60;
+    },
+});
