d0e0fc2bb22ddc5b934881efa1387f976869a44a
[hyunba] <dls272@naver.com>
2022-01-30 21:43:20 +0900

    Reordering Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -36,8 +38,22 @@ const toDos = ["a", "b", "c", "d", "e", "f"];
 
 // dragHandleProps는 해당 지정영역을 눌러야 이동할수있게함
 // dragableProps는 해당 영역의 내용들이 옮겨짐
+// atom의 값을 가져오려면 useRecoilValue를 사용하면되지만 atom의 값 뿐만 아니라 atom을 수정하는 함수까지 부르고 싶다면 useRecoilState를 사용.
 function App() {
-  const onDragEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState)
+  const onDragEnd = ({draggableId, destination, source}:DropResult) => {
+    if (!destination) return;
+
+    setToDos(oldToDos =>{
+      const copyToDos = [...oldToDos];
+      // 1) Delete item
+      copyToDos.splice(source.index, 1)
+      // 2) Put back item
+      copyToDos.splice(destination?.index, 0, draggableId)
+
+      return copyToDos;
+    })
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
@@ -45,14 +61,11 @@ function App() {
           <Droppable droppableId="one">
             {(element)=>
               <Board ref={element.innerRef} {...element.droppableProps}>
-                {toDos.map((toDo, index) => (<Draggable draggableId={toDo} index={index}>
+                {toDos.map((toDo, index) => (<Draggable key={toDo} draggableId={toDo} index={index}>
                   {(element) => (<Card ref={element.innerRef} {...element.draggableProps} {...element.dragHandleProps}>
                     <span>❤️</span>
                     {toDo}</Card>)}
                 </Draggable>))}
-                <Draggable draggableId="second" index={1}>
-                  {(element) => <Card ref={element.innerRef} {...element.draggableProps} {...element.dragHandleProps}>Two</Card>}
-                </Draggable>
                 {element.placeholder}
               </Board>
             }
